8f1340ce02f369d2bdacc52e90c193e8fbfbbcad
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-03 10:39:26 +0530

    first commit

---
+++ src/main/java/com/week3/DataJPAMapping/DataJpaMappingApplication.java
@@ -0,0 +1,13 @@
+package com.week3.DataJPAMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/week3/DataJPAMapping/DataJpaMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week3.DataJPAMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
